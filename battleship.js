b0dc4e9299eea34cd3be108becea29c06697f092
Akouvi <kouedjinl@gmail.com>
2016-04-18 15:14:52 -0700

    Added files via upload

---
+++ battleship.js
@@ -0,0 +1,16 @@
+var view = {
+	displayMessage: function(msg){
+					var messageArea = document.getElementById("messageArea");
+					messageArea.innerHTML = msg;
+	},
+
+
+	displayHit: function(){
+
+	},
+
+	displayMiss: function(){
+
+	}
+
+};
